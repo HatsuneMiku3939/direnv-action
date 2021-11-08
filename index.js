b385c2fb3428e598dd8308f2e282a299cfbf8656
Brandon Zeeb <brandon.zeeb@blockfi.com>
2021-11-07 21:54:43 -0500

    try to use one from dist

--- index.js
+++ index.js
@@ -2,32 +2,12 @@ const core = require('@actions/core');
 const cp = require("child_process");
 const fs = require("fs");
 
-async function install() {
-  try {
-    cp.execSync(`command -v direnv`, { encoding: "utf-8"} )
-  }
-  catch (error) {
-    core.info(`Download direnv via apt-get...`);
-    try {
-      cp.execSync('sudo apt-get install -y direnv', { encoding: "utf-8" });
-      core.info(`  Success!`);
-    }
-    catch (error2) {
-      core.info(`  Failure... `)
-      core.info(`Download direnv via curl...`)
-      cp.execSync('curl -sfL https://direnv.net/install.sh | bash > /dev/null 2>&1', { encoding: "utf-8" });
-      core.info(`  Success!`);
-    }
-  }
-}
-
 // most @actions toolkit packages have async methods
 async function run() {
   try {
     // If there's no .envrc, skip all this
     if(fs.existsSync(".envrc")) {
-      install();
-      cp.execSync('direnv allow', { encoding: "utf-8" });
+      cp.execSync('./direnv.linux-amd64 allow', { encoding: "utf-8" });
       const envs = JSON.parse(cp.execSync('direnv export json', { encoding: "utf-8" }));
 
       Object.keys(envs).forEach(function (name) {
